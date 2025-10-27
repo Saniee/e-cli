953204d73f31899a9593f0d5defbc52604a9fea1
Saniee <asamsku10@gmail.com>
2025-10-27 22:10:34 +0100

    Code Refactor

--- src/funcs.rs
+++ src/funcs.rs
@@ -18,42 +18,42 @@ pub fn download(data: Vec<Post>, lower_quality: bool) -> f64 {
 
         // println!("Starting download of {}-{}.{}",artist_name, post.id, post.file.ext);
 
-        if !path.exists() {
-            let file_size: f64;
-            if lower_quality {
-                file_size = lower_quality_dl_file(&post, &artist_name);
-                dl_size += file_size;
-            } else {
-                match &post.file.url {
-                    Some(url) => {
-                        file_size =
-                            download_file(url, &post.file.ext, post.id, &artist_name);
-                        dl_size += file_size;
-                    }
-                    None => {
-                        println!(
-                            "Cannot download post {}-{} due to it missing a file url",
-                            artist_name, post.id
-                        );
-                        file_size = 0.0;
-                        dl_size += file_size;
-                    }
-                }
-            }
-
-            println!(
-                "Downloaded {}-{}.{}! File size: {:.2} MB",
-                artist_name,
-                post.id,
-                post.file.ext,
-                file_size / 1024.0 / 1024.0
-            );
-        } else {
+        if path.exists() {
             println!(
                 "File {}-{}.{} already Exists!",
                 artist_name, post.id, post.file.ext
             )
         }
+
+        let file_size: f64;
+        if lower_quality {
+            file_size = lower_quality_dl_file(&post, &artist_name);
+            dl_size += file_size;
+        } else {
+            match &post.file.url {
+                Some(url) => {
+                    file_size =
+                        download_file(url, &post.file.ext, post.id, &artist_name);
+                    dl_size += file_size;
+                }
+                None => {
+                    println!(
+                        "Cannot download post {}-{} due to it missing a file url",
+                        artist_name, post.id
+                    );
+                    file_size = 0.0;
+                    dl_size += file_size;
+                }
+            }
+        }
+
+        println!(
+            "Downloaded {}-{}.{}! File size: {:.2} MB",
+            artist_name,
+            post.id,
+            post.file.ext,
+            file_size / 1024.0 / 1024.0
+        );
     };
     
     dl_size
@@ -77,19 +77,19 @@ pub fn download_file(
 }
 
 pub fn lower_quality_dl_file(post: &Post, artist_name: &str) -> f64 {
-    if post.sample.has {
-        if let Some(lower_quality) = &post.sample.alternates.lower_quality {
-            if lower_quality.media_type == "video" {
-                download_file(&lower_quality.urls[0], &post.file.ext, post.id, artist_name)
-            } else if let Some(sample_url) = &post.sample.url {
-                download_file(sample_url, &post.file.ext, post.id, artist_name)
-            } else {
-                println!(
-                    "Cannot download post {}-{} due it not having any file url.",
-                    artist_name, &post.id
-                );
-                0.0
-            }
+    if !post.sample.has {
+        println!(
+            "Cannot download post {}-{} due it not having any file url.",
+            artist_name, &post.id
+        );
+        return 0.0
+    } else if let Some(url) = &post.file.url {
+        return download_file(url, &post.file.ext, post.id, artist_name)
+    }
+    
+    if let Some(lower_quality) = &post.sample.alternates.lower_quality {
+        if lower_quality.media_type == "video" {
+            download_file(&lower_quality.urls[0], &post.file.ext, post.id, artist_name)
         } else if let Some(sample_url) = &post.sample.url {
             download_file(sample_url, &post.file.ext, post.id, artist_name)
         } else {
@@ -99,8 +99,8 @@ pub fn lower_quality_dl_file(post: &Post, artist_name: &str) -> f64 {
             );
             0.0
         }
-    } else if let Some(url) = &post.file.url {
-        download_file(url, &post.file.ext, post.id, artist_name)
+    } else if let Some(sample_url) = &post.sample.url {
+        download_file(sample_url, &post.file.ext, post.id, artist_name)
     } else {
         println!(
             "Cannot download post {}-{} due it not having any file url.",
